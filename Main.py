1c69350288282b96022b908c867ef74835bb359e
Till Hanke <tillvonule@yahoo.de>
2018-09-25 13:55:18 +0200

    timer reset button added and fixed the issue with the double time count after starting twice without pausing in between

--- Main.py
+++ Main.py
@@ -19,13 +19,16 @@ class Timer:
     '''
 
     def __init__(self, master, col, row, countdown):
+        self.update_id = None
         self.master = master
         self.countdown = countdown
         self.running = False
         if countdown:
             self.timer_label = Label(master, text="Down counting timer (set start)")
+            self.timer_file = "Output/timer_down.txt"
         else:
             self.timer_label = Label(master, text="Up counting timer (set start)")
+            self.timer_file = "Output/timer_up.txt"
         self.timer_label.grid(columnspan=4, column=col, row=row)
 
         #Entry
@@ -45,7 +48,12 @@ class Timer:
         self.timer_pause = Button(master, command=self.pause, text="Pause")
         self.timer_pause.grid(columnspan=2, column=col+2, row=row+3, sticky=E+W)
 
+        self.timer_reset = Button(master, command=self.reset, text="Reset")
+        self.timer_reset.grid(columnspan=2, column=col + 4, row=row + 3, sticky=E + W)
+
     def start(self):
+        if self.running:
+            self.master.after_cancel(self.update_id)
         self.running = True
         if self.countdown:
             self.minutes, self.seconds = self.timer_set.get().split(":")
@@ -65,7 +73,7 @@ class Timer:
                 else:
                     self.seconds = str(59)
                     self.minutes = str(int(self.minutes)-1)
-                with open("Output/timer_down.txt", "w") as f:
+                with open(self.timer_file, "w") as f:
                     f.write("%02d" % (int(self.minutes),)+":"+"%02d" % (int(self.seconds),))
             else:
                 if int(self.seconds) < 59:
@@ -76,12 +84,21 @@ class Timer:
                 with open("Output/timer_up.txt", "w") as f:
                     f.write("%02d" % (int(self.minutes),)+":"+"%02d" % (int(self.seconds),))
             self.current_time.set("%02d" % (int(self.minutes),)+":"+"%02d" % (int(self.seconds),))
-            self.master.after(1000, self.update_timer)
+            self.update_id = self.master.after(1000, self.update_timer)
 
     def pause(self):
         self.running = False
+        self.master.after_cancel(self.update_id)
         self.timer_set.delete(0, END)
         self.timer_set.insert(0, "%02d" % (int(self.minutes),)+":"+"%02d" % (int(self.seconds),))
+
+    def reset(self):
+        self.running = False
+        self.master.after_cancel(self.update_id)
+        self.timer_set.delete(0, END)
+        self.current_time.set("%02d" % (0,) + ":" + "%02d" % (0,))
+        with open(self.timer_file, "w") as f:
+            f.write("%02d" % (0,) + ":" + "%02d" % (0,))
 
 
 class Team:
@@ -211,7 +228,7 @@ class ScoreBoardGUI:
         self.setup_teams()
 
         self.timer_up = Timer(master=self.master, col=col, row=row+3, countdown=False)
-        self.timer_down = Timer(master=self.master, col=col+4, row=row+3, countdown=True)  # takes 4 rows!
+        self.timer_down = Timer(master=self.master, col=col+6, row=row+3, countdown=True)  # takes 4 rows!
 
         self.penalty_column = col
         self.penalty_row = row + 7
