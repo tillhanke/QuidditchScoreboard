9c6bc4cc0aa3a841c86edefa0d49273892fe6190
Sarah <sarah.kempf@gmx.de>
2019-04-30 02:26:19 +0200

    Added first GUI parts for C# Version of Scoreboard System

---
+++ ScoreBoard/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeKeeper1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Settings settingWindow = new Settings();
+            settingWindow.Show();
+        }
+
+        private void GetExtraTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExtraTimer ObjExtraTimer = new ExtraTimer();
+            ObjExtraTimer.Show();
+        }
+    }
+}
